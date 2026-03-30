b05af1a3fe1a7c6ad510825ae3d1da5287a3ce2f
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-03-30 20:36:29 +0300

    update: added the tokenizer file with the generator function for data btching to the trainer

--- tokenizer.py
+++ tokenizer.py
@@ -7,6 +7,13 @@ from tokenizers.normalizers import NFKC
 from datasets import load_dataset
 
 
+dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
+
+def get_training_corpus():
+    for i in range(0, len(dataset), 1000):
+        yield dataset[i : i + 1000]["text"]
+
+
 
 
 
