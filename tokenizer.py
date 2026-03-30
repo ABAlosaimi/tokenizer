f7c4d6dd1c66365868dcbcd7cded831c65e62f39
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-03-30 20:21:06 +0300

    first commit

---
+++ tokenizer.py
@@ -0,0 +1,12 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers import normalizers
+from tokenizers.normalizers import NFKC
+from datasets import load_dataset
+
+
+
+
+
